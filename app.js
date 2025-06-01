7dd9973db7e5bb19d1c1a2e8e6cbabc1fcaaccb4
hamnam1 <hamed.namazi@hotmail.com>
2025-06-01 11:38:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const http = require('http');
-const axios = require('axios');
+// trasig kod - saknar http-modulen
+const somethingWrong = () => {
+  console.log("This app won't work.");
+};
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
+somethingWrong();
 
-const server = http.createServer(async (req, res) => {
-  try {
-    const jokeResponse = await axios.get('https://official-joke-api.appspot.com/random_joke');
-    const joke = jokeResponse.data;
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1>${joke.setup}</h1><p>${joke.punchline}</p>`);
-  } catch (error) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Error fetching joke');
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
